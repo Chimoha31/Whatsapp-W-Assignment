2770134a293c3e1455cf5cc098c0d7feabefa3b4
Chimoha31 <mokochii1108@gmail.com>
2022-06-07 22:55:37 -0700

    Home & Login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Container>
+      <Row>
+        <Col
+          md={6}
+          className="d-flex flex-direction-column align-items-center justify-content-center"
+        >
+          <div>
+            <h1>Let's Chat with your friends</h1>
+            <p>This is chat app </p>
+            <LinkContainer to="/login">
+              <Button variant="success">
+                Get Started
+                <i className="fas fa-comments home-message-icon"></i>
+              </Button>
+            </LinkContainer>
+          </div>
+        </Col>
 
-export default Home
+        <Col md={6} className="home__bg home"></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
